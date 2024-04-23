68b860981acb96599e651cac92e6761e4ba9484a
Hawtian Wang <twistoy.wang@gmail.com>
2024-04-23 22:49:16 +0800

    chore(cache): remove ?? cache

--- modules/shared/core.nix
+++ modules/shared/core.nix
@@ -14,7 +14,6 @@ in {
     trusted-users = [user.name];
 
     substituters = [
-      "https://mirrors.cernet.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://twistoy.cachix.org"
       "https://nix-community.cachix.org"
