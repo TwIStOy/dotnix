92bffd1e3d6bff37c24f937d7bfb5c2b479b5b79
Hawtian Wang <twistoy.wang@gmail.com>
2024-12-19 09:31:49 +0800

    feat: use desktop

--- hosts/nagato/modules.nix
+++ hosts/nagato/modules.nix
@@ -8,9 +8,14 @@ in {
     desktop.neovide.extraSettings.font.size = 18;
     apps.zed.buffer_font_size = 18;
     apps.zed.ui_font_size = 16;
-  };
 
-  dotnix.services.tailscale.enable = true;
+    services.tailscale = {
+      enable = true;
+      extraUpFlags = [
+        "--advertise-tags=tag:desktop"
+      ];
+    };
+  };
 
   networking.hostName = hostname;
   networking.computerName = hostname;
