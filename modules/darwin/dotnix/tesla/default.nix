b69f230804f96cf36bfde9178937454d0ef657d7
Hawtian Wang <twistoy.wang@gmail.com>
2025-04-17 16:12:40 +0800

    feat: init tesla specific configs

---
+++ modules/darwin/dotnix/tesla/default.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  dotnix-utils,
+  ...
+}: let
+  cfg = config.dotnix.tesla;
+in {
+  options.dotnix.tesla = {
+    enable = lib.mkEnableOption "Enable module dotnix.tesla";
+  };
+
+  config = lib.mkIf cfg.enable {
+  };
+}
